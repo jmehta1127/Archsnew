36284b4b2e552e9357a015ca4c01d244f4a4b35f
Jay Mehta <jmehta@usgbc.org>
2015-07-16 14:57:46 -0400

    removing piston handler

--- archs/handlers.py
+++ archs/handlers.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from piston.handler import BaseHandler
+#from piston.handler import BaseHandler
 from django.http import HttpResponse
 import string
 from django.core.exceptions import ValidationError
 import subprocess
 import sys, httplib2, json, os;
 
-class getBuildingsHandler(BaseHandler):
-    allowed_methods = ('GET',)
+#class getBuildingsHandler(BaseHandler):
+    #allowed_methods = ('GET',)
     #model = Building
 
-    def read(self, request):
+    #def read(self, request):
         #try:
-      return "hello world"
+     #return "hello world"
             #return Building.objects
         #except Building.DoesNotExist:
             #return "No buildings available"
--- archs/urls.py
+++ archs/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from . import views
-from handlers import getBuildingsHandler
+#from handlers import getBuildingsHandler
 
 urlpatterns = patterns('',
     url(r'^getBuildings/$', views.get_building, name='get_building'),
-    url(r'^getBuilding/$', getBuildings_Handler),
+    #url(r'^getBuilding/$', getBuildings_Handler),
 )
